4743e8df3e2b649a1ebd29af7a8cc8e37d13ad79
GamemakerPixel <57157106+GamemakerPixel@users.noreply.github.com>
2019-11-16 18:01:18 -0500

    Update Player.gd

--- Basic 2D Movement and Collisions/Player.gd
+++ Basic 2D Movement and Collisions/Player.gd
@@ -1,10 +1,10 @@
 extends KinematicBody2D #Allows functions from KinematicBody2D to be used along with its parents' functions
 
-export (int) var speed #export - allows this variable to be edited in the inspector, (int) - states that this variable is an integer and will only allow the value put in to be an integer
-var Velocity = Vector2() #Vector2s store an x and y value, in this case, the left and right motion (x) and the upward and downward motion (y)
+export (int) var speed #export - allows this variable to be edited in the inspector, (int) - states that this variable is an integer and will only allow the given value to be an integer
+var Velocity = Vector2() #Vector2s store an x and y value, in this case, the left and right velocity (x) and the upward and downward velocity (y)
 
 func _physics_process(delta): #Called every frame
-	if Input.is_action_pressed("ui_right"): #True while the chosen input (right arrow key) is pressed
+	if Input.is_action_pressed("ui_right"): #True every frame while the chosen input (right arrow key) is pressed
 		Velocity.x = speed
 	elif Input.is_action_pressed("ui_left"): #if the original if statement (line 7) and the elif statement (this line) are both false, the code in the else statement (line 11) will run
 		Velocity.x = -speed
